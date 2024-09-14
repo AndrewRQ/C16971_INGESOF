2b0f515a42572263461c1144b0d6490a4c3c5418
ANDREW.ROSALES <ANDREW.ROSALES@ucr.ac.cr>
2024-09-14 17:58:02 -0600

    Laboratorio 5 Terminado

---
+++ MiPrimerProyecto/backend-lab-C16971/backend-lab-C16971/Controllers/PaisesController.cs
@@ -0,0 +1,23 @@
+﻿using backend_lab_C16971.Handlers;
+using backend_lab_C16971.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_C16971.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisesHandler _paisesHandler;
+        public PaisesController()
+        {
+            _paisesHandler = new PaisesHandler();
+        }
+        [HttpGet]
+        public List<PaisesModel> Get()
+        {
+            var paises = _paisesHandler.ObtenerPaises();
+            return paises;
+        }
+    }
+}
---
+++ MiPrimerProyecto/backend-lab-C16971/backend-lab-C16971/Models/PaisesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_lab_C16971.Models
+{
+    public class PaisesModel
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required string Continente { get; set; }
+        public required string Idioma { get; set; }
+    }
+}
